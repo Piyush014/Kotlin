a4e5dc3263995b86a2d1471c01b187b1ddf72f48
Piyush014 <109540904+Piyush014@users.noreply.github.com>
2024-07-23 19:15:29 +0530

    Reflections in Kotlin

--- 07_Reflection.kt
+++ 07_Reflection.kt
@@ -1,2 +1,9 @@
-package Kotlin
+// Reflection in kotlin
+// Reflection is a set of language and library features that allows you to introspect the structure of your program at runtime.
+// Functions and properties are first-class citizens in Kotlin, and the ability to introspect them (for example, learning the name or the type of a property or function at runtime) is essential when using a functional or reactive style.
+// JVM dependency
 
+// gradle
+// dependencies {
+//    implementation(kotlin("reflect"))
+//}
